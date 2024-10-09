5fdb871bba3065ea8fc783d6632db3fb5a9ce738
Corfaya <lauracorrado0696@gmail.com>
2024-10-09 17:42:57 +0200

    [FIX] wrong name function: fixed

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,13 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Type extends Model
 {
     use HasFactory;
 
+    public static function generateSlug($title) {
+        return Str::slug($title, '-');
+    }
+
     // 1 to m con tabella projects
-    public function type() {
+    public function projects() {
         return $this->hasMany(Project::class);
     }
 }
