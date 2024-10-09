ce934ba19f139e13c777b20a0aa7f669d11defbf
Corfaya <lauracorrado0696@gmail.com>
2024-10-09 17:43:31 +0200

    [UPD+ DatabaseSeeder filled with porject's seeders

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +15,14 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = ['Sviluppo Full-Stack', 'Sviluppo Frontend', 'Sviluppo Backend', 'UX/UI', "Gestione Database e analisi dati"];
+
+        foreach($types as $t) {
+            $type = new Type();
+            $type->name = $t;
+            $type->slug = Type::generateSlug($t);
+
+            $type->save();
+        }
     }
 }
